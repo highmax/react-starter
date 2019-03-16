4d8386d470efbc06807a7f17f98d68bc66591db8
Alejandro Navarro <alejo@MacBook-Pro-de-Alejandro.local>
2019-03-16 16:44:35 -0500

    added linter and code prettier

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,15 @@
-import React from "react";
-import { hot } from "react-hot-loader";
+import React from 'react';
+import { hot } from 'react-hot-loader';
 
-import style from "./app.css";
+import style from './app.css';
 
-const App = () => {
-  return <div className={style.app}>react Starter 🚀 </div>;
-};
+const App = () => (
+  <div className={style.app}>
+    react Starter
+    <span role="img" aria-label="rocket">
+      🚀
+    </span>
+  </div>
+);
 
-export default hot(module)(App)
+export default hot(module)(App);
